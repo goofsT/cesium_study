2f1dffe4e110c8d4f3cfa61bebbeb28f102c788e
AmazingTcc <445632625@qq.com>
2024-04-09 22:16:29 +0800

    环境搭建

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import * as Cesium from 'cesium';
+import {onMounted} from "vue";
+
+onMounted(()=>{
+  initView()
+})
+const initView=()=>{
+  const viewer=new Cesium.Viewer('map-container')
+}
+
+</script>
+
+<template>
+  <div id="map-container"></div>
+</template>
+<style lang="scss" scoped>
+#map-container{
+  height: 100vh;
+  width:100vw;
+}
+</style>
